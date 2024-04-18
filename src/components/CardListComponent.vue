d64657d74825b8eeaf0e01649f7719376038b091
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-04-18 15:48:59 +0200

    add cardlist component

---
+++ src/components/CardListComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div :id="type" class="container">
+        <div class="row g-3" v-if="!store.isLoading">
+            <div class="col-12 col-md-6 col-lg-3" v-for="(watch, index) in store[type]" :key="index">
+                <CardComponent :title="watch.title" :oTitle="watch.oTitle" :language="watch.language"
+                    :vote="watch.vote" :image="watch.image" />
+            </div>
+        </div>
+        <ApiLoader v-else />
+    </div>
+</template>
+
+<script>
+import { store } from '../store.js';
+import CardComponent from './CardComponent.vue';
+import ApiLoader from './ApiLoader.vue';
+export default {
+    name: 'CardListComponent',
+    props: ['type'],
+    components: {
+        CardComponent,
+        ApiLoader
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,39 +1,22 @@
 <template>
     <main>
-        <h2 v-if="!store.movies.length" class=" text-center fw-bold fs-1 p-5  ">Immettere il Titolo del Film o della Serie TV nella barra di ricerca in alto a destra</h2>
+        <h2 v-if="!store.movies.length" class=" text-center fw-bold fs-1 p-5  ">Immettere il Titolo del Film o della
+            Serie TV nella barra di ricerca in alto a destra</h2>
 
         <h2 class="p-5" v-if="store.movies.length">Film</h2>
-        <div id="movie" class="container">
-            <div class="row g-3" v-if="!store.isLoading">
-                <div class="col-12 col-md-6 col-lg-3" v-for="(movie, index) in store.movies" :key="index">
-                    <CardComponent class="movie" :title="movie.title" :oTitle="movie.oTitle" :language="movie.language"
-                        :vote="movie.vote" :image="movie.image" />
-                </div>
-            </div>
-            <ApiLoader v-else />
-        </div>
+        <CardListComponent type="movies" />
         <h2 class="p-5" v-if="store.movies.length">Serie TV</h2>
-        <div class="container">
-            <div class="row g-3" v-if="!store.isLoading">
-                <div class="col-12 col-md-6 col-lg-3" v-for="(tv, index) in store.tvs" :key="index">
-                    <CardComponent :title="tv.title" :oTitle="tv.oTitle" :language="tv.language" :vote="tv.vote"
-                        :image="tv.image" />
-                </div>
-            </div>
-            <ApiLoader v-else />
-        </div>
+        <CardListComponent type="tvs" />
     </main>
 </template>
 
 <script>
 import { store } from '../store.js';
-import CardComponent from './CardComponent.vue';
-import ApiLoader from './ApiLoader.vue';
+import CardListComponent from './CardListComponent.vue';
 export default {
     name: 'MainComponent',
     components: {
-        CardComponent,
-        ApiLoader
+        CardListComponent
     },
     data() {
         return {
@@ -44,7 +27,7 @@ export default {
 </script>
 
 <style lang="scss">
-#movie {
+#movies {
     img {
         height: 400px;
     }
